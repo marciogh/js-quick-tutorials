92532bc9eb7c63390a3139a036c4ee5c7913e2e0
Marcio Ghiraldelli <mghiraldelli@atlassian.com>
2020-08-03 10:36:53 +1000

    express

--- node_basics/be/app.js
+++ node_basics/be/app.js
@@ -4,11 +4,13 @@ const app = express()
 app.use(cors())
  
 app.get('/students/create', function (req, res) {
-  res.code(201).send('Hello World from server')
+  // insert student in database
+  res.code(201).send('Student created')
 })
 
 app.get('/students/delete', function (req, res) {
-    res.send('Hello World 2 from server')
+    // delete student from database
+    res.send('Student delete')
 })
 
 app.listen(8080)
